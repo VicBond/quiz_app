e74ef32435b7a0d22993810bede01c4b65d05a7f
Victor Bondarenko <nirasimha@gmail.com>
2022-03-04 10:57:16 -0500

    add for each loop to check all answers

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ form.addEventListener('submit', e => {
 
   let score = 0;
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  //check what answers are correct
+
+  userAnswers.forEach((answer, index) => {
+    if(answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+  
+
 });
